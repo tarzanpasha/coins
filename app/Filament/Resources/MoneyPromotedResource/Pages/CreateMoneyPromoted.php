7b1b777a47eaaccd69c8af6e767ca46bafdd7752
xfile <atreidesp@gmail.com>
2022-07-04 23:29:27 +0300

    таблица размещений

---
+++ app/Filament/Resources/MoneyPromotedResource/Pages/CreateMoneyPromoted.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MoneyPromotedResource\Pages;
+
+use App\Filament\Resources\MoneyPromotedResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMoneyPromoted extends CreateRecord
+{
+    protected static string $resource = MoneyPromotedResource::class;
+}
